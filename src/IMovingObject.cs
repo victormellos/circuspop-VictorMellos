e3c98da7eeebea343871c9bdb222638b0c88a70f
Victor Mellos <victormellos@outlook.com>
2026-03-23 17:07:55 -0300

    Delete src directory

--- src/IMovingObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace CircusPop;
-
-interface IMovingObject
-{
-    Vector2 Position { get; }
-    Vector2 Velocity { get; }
-}
--- src/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿using var game = new CircusPop.Game1();
-game.Run();
