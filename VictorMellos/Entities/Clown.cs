81fbccf594a3b0e89a6f18085adab7331b9479ba
victormellos <victormellos@outlook.com>
2026-03-26 17:00:45 -0300

    FIX: New even better code directory management

---
+++ VictorMellos/Entities/Clown.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+
+namespace VictorMellos
+{
+    public class Clown : Character
+    {
+    
+    
+
+    public Clown(Vector2 initialPosition)
+    : base(initialPosition, Vector2.Zero)
+    {
+    }
+
+
+
+    }
+}
