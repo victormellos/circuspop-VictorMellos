b7fb53500c6a06ae3f46f287748fa6fb6c022d7a
victormellos <victormellos@outlook.com>
2026-04-01 15:42:24 -0300

    FEAT(Screens): Add GameScreen

--- VictorMellos/Core/ClownGame.cs
+++ VictorMellos/Core/ClownGame.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Audio;
 using System.Collections.Generic;
+using System.Data;
 
 
 namespace VictorMellos;
@@ -11,15 +12,16 @@ namespace VictorMellos;
 public class ClownGame : Game
 {
     private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+    // private SpriteBatch _spriteBatch;
 
     private List<Player> players;
 
-    private SoundEffect bounce;
+    // private SoundEffect bounce;
     
-    private SpriteFont _textFont;
+    // private SpriteFont _textFont;
 
-
+    private GameScreen gameScreen;
+    private GameState gameState = GameState.Playing;
 
     public ClownGame()
     {
@@ -48,143 +50,48 @@ public class ClownGame : Game
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        bounce = Content.Load<SoundEffect>("bounce");
-
-        _textFont = Content.Load<SpriteFont>("ScoreFont"); 
-
-        foreach (var player in players)
+        switch (gameState)
         {
-            player.Clown.Sprite = new Sprite();
-            player.Clown.Sprite.Texture = Content.Load<Texture2D>("clown");
-
-            
-
-            player.Trampoline.Sprite = new Sprite();
-            player.Trampoline.Sprite.Texture = Content.Load<Texture2D>("Trampoline");
-
-                player.Trampoline.Position = new Vector2(
-                    player.Trampoline.Position.X,
-                    GraphicsDevice.Viewport.Height - player.Trampoline.Height
-                );
+            case GameState.Playing:
+                gameScreen = new GameScreen(GraphicsDevice, Content, players);
+                gameScreen.LoadContent();
+                break;
         }
- 
+        // _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // bounce = Content.Load<SoundEffect>("bounce");
+
+        // gameScreen = new GameScreen(GraphicsDevice, Content, players);
+
+        // _textFont = Content.Load<SpriteFont>("ScoreFont"); 
+
+
 
     }
 
     protected override void Update(GameTime gameTime)
-    /*
-    Ordem para verificar:
-    Input
-    Movimento
-    Física (gravidade, velocidade, etc.)
-    Colisão
-    Resposta da colisão
-    */
+
     {
-        foreach (var player in players)
+        switch (gameState)
         {
-            player.HandleInput();
-        }
-        
-       foreach (var player in players)
-        {
-            var clown = player.Clown;
-            if (clown == null)
-                continue;
-
-            var trampoline = player.Trampoline;
-
-            // 1. Clamp do trampolim
-            trampoline.Position.X = MathHelper.Clamp(
-                trampoline.Position.X,
-                0,
-                GraphicsDevice.Viewport.Width - trampoline.Width
-            );
-
-            // 2. Movimento do clown
-            clown.Position += clown.Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            // 3. Gravidade
-            clown.Velocity.Y += 16f;
-
-            // 4. Limites da tela
-            int maxX = GraphicsDevice.Viewport.Width - clown.Width;
-            int maxY = GraphicsDevice.Viewport.Height - clown.Height;
-
-            if (clown.Position.Y > maxY)
-            {
-                player.Score.AddPoints(-1200);
-                player.Lives --;
-
-                if (player.Lives <= 0)
-                {
-                    player.IsAlive = false;
-                }
-                
-                clown.Position = trampoline.Position + new Vector2(trampoline.Height, 0);
-
-            }
-
-            if (clown.Position.X > maxX)
-            {
-                clown.Velocity.X *= -1;
-                clown.Position.X = maxX;
-            }
-            else if (clown.Position.X < 0)
-            {
-                clown.Velocity.X *= -1;
-                clown.Position.X = 0;
-            }
-
-
-
-
-            // 6. Colisão
-            if (Collision.Intersects(trampoline, clown)){
-                int angle = trampoline.Bounds.Center.X - clown.Bounds.Center.X + 90;
-
-                clown.Velocity.X = 980f * (float)Math.Cos(MathHelper.ToRadians(angle));
-                clown.Velocity.Y = 980f * (float)Math.Sin(MathHelper.ToRadians(angle)) * -1;
-
-                bounce.Play();
-            }
+            case GameState.Playing:
+                gameScreen.Update(gameTime);
+                break;
+            default:
+                Console.Write("Erro esquisitão");
+                break;
         }
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        _spriteBatch.Begin();
-        
-        Vector2 scorePosition = new Vector2(10, 10);
-        float offsetY = 0f;
-
-        foreach (var player in players)
+        switch (gameState)  
         {
-            if (player.Clown != null)
-            {
-                _spriteBatch.Draw(player.Clown.Sprite.Texture, player.Clown.Position, Color.White);
-            }
-            
-            _spriteBatch.Draw(player.Trampoline.Sprite.Texture, player.Trampoline.Position, Color.White);
-
-            string scoreText = $"{player.Name} : {player.Score.Points}\nVidas: {player.Lives}";
-            
-            _spriteBatch.DrawString(
-                _textFont,
-                scoreText,
-                new Vector2(scorePosition.X, scorePosition.Y + offsetY),
-                Color.White
-            );
-
-            offsetY += 30f; 
+            case GameState.Playing:
+                gameScreen.Draw();
+                break;
         }
-
-        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
---
+++ VictorMellos/Core/Screens/GameScreen.cs
@@ -0,0 +1,167 @@
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System;
+
+namespace VictorMellos;
+
+class GameScreen
+{
+    private SoundEffect bounce;
+    private SpriteFont _textFont;
+    private SpriteBatch _spriteBatch;
+
+    private GraphicsDevice _graphicsDevice;
+    
+    private List<Player> players;
+    private ContentManager _content;
+
+    public GameScreen(GraphicsDevice graphicsDevice, ContentManager content, List<Player> players)
+    {
+        _graphicsDevice = graphicsDevice;
+        _content = content;
+        this.players = players;
+    }
+
+    public void LoadContent()
+    {  
+        
+        _spriteBatch = new SpriteBatch(_graphicsDevice);
+
+        bounce = _content.Load<SoundEffect>("bounce");
+        _textFont = _content.Load<SpriteFont>("ScoreFont");
+                foreach (var player in players)
+        {
+            player.Clown.Sprite = new Sprite();
+            player.Clown.Sprite.Texture = _content.Load<Texture2D>("clown");
+
+            
+
+            player.Trampoline.Sprite = new Sprite();
+            player.Trampoline.Sprite.Texture = _content.Load<Texture2D>("Trampoline");
+
+                player.Trampoline.Position = new Vector2(
+                    player.Trampoline.Position.X,
+                    _graphicsDevice.Viewport.Height - player.Trampoline.Height
+                );
+        }
+ 
+    }
+
+    public void Update(GameTime gameTime)
+    {
+            /*
+    Ordem para verificar:
+    Input
+    Movimento
+    Física (gravidade, velocidade, etc.)
+    Colisão
+    Resposta da colisão
+    */
+            foreach (var player in players)
+        {
+            player.HandleInput();
+        }
+        
+        foreach (var player in players)
+        {
+            var clown = player.Clown;
+            if (clown == null)
+                continue;
+
+            var trampoline = player.Trampoline;
+
+            // 1. Clamp do trampolim
+            trampoline.Position.X = MathHelper.Clamp(
+                trampoline.Position.X,
+                0,
+                _graphicsDevice.Viewport.Width - trampoline.Width
+            );
+
+            // 2. Movimento do clown
+            clown.Position += clown.Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // 3. Gravidade
+            clown.Velocity.Y += 16f;
+
+            // 4. Limites da tela
+            int maxX = _graphicsDevice.Viewport.Width - clown.Width;
+            int maxY = _graphicsDevice.Viewport.Height - clown.Height;
+
+            if (clown.Position.Y > maxY)
+            {
+                player.Score.AddPoints(-1200);
+                player.Lives --;
+
+                if (player.Lives <= 0)
+                {
+                    player.IsAlive = false;
+                }
+                
+                clown.Position = trampoline.Position + new Vector2(trampoline.Height, 0);
+
+            }
+
+            if (clown.Position.X > maxX)
+            {
+                clown.Velocity.X *= -1;
+                clown.Position.X = maxX;
+            }
+            else if (clown.Position.X < 0)
+            {
+                clown.Velocity.X *= -1;
+                clown.Position.X = 0;
+            }
+
+
+
+
+            // 6. Colisão
+            if (Collision.Intersects(trampoline, clown)){
+                int angle = trampoline.Bounds.Center.X - clown.Bounds.Center.X + 90;
+
+                clown.Velocity.X = 980f * (float)Math.Cos(MathHelper.ToRadians(angle));
+                clown.Velocity.Y = 980f * (float)Math.Sin(MathHelper.ToRadians(angle)) * -1;
+
+                bounce.Play();
+            }
+        }
+    }
+
+    public void Draw()
+    {
+        _graphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+        
+        Vector2 scorePosition = new Vector2(10, 10);
+        float offsetY = 0f;
+
+        foreach (var player in players)
+        {
+            if (player.Clown != null)
+            {
+                _spriteBatch.Draw(player.Clown.Sprite.Texture, player.Clown.Position, Color.White);
+            }
+            
+            _spriteBatch.Draw(player.Trampoline.Sprite.Texture, player.Trampoline.Position, Color.White);
+
+            string scoreText = $"{player.Name} : {player.Score.Points}\nVidas: {player.Lives}";
+            
+            _spriteBatch.DrawString(
+                _textFont,
+                scoreText,
+                new Vector2(scorePosition.X, scorePosition.Y + offsetY),
+                Color.White
+            );
+
+            offsetY += 30f; 
+        }
+
+        _spriteBatch.End();
+
+
+    }
+}
